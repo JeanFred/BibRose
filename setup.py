93797af9b3f57327be3ae0137da0c6412963e013
Jean-Frédéric <JeanFred1@gmail.com>
2012-04-20 01:32:53 +0200

    Add setup script and config

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='BibRose',
+    version      = '0.1',
+    description  = '',
+    py_modules = ['OaiRecordHandling', 'OaiServerTools', 'GeneralProcess', 'CommonsFunctions'],
+    author       = 'Jean-Frederic',
+    author_email = 'jeanfred@github',
+    license      = 'Biopython License',
+    install_requires     = ['oaipmh', 'pywikipedia'],
+    data_files=[('', ['oai_servers.cfg'])],
+    )
