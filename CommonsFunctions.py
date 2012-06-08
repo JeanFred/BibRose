b9233529071901313853769089307bbdf4861fa0
Jean-Frédéric <JeanFred1@gmail.com>
2012-06-08 22:08:03 +0200

    Modify method build_Commons_title
     - Rename method to match convention
     - Use other method to get title

--- CommonsFunctions.py
+++ CommonsFunctions.py
@@ -5,11 +5,13 @@
 """
 __authors__ = 'User:Jean-Frédéric'
 
+from OaiRecordHandling import retrieve_title
 
-def buildCommonsTitle(record):
+
+def build_Commons_title(record):
     """Builds the file title for Wikimedia Commons for the given record
 
     The naming convention is: "<Title> - Fonds Trutat.jpg"
     """
     recordTitle = record[1]['title'][0]
-    return "%s - Fonds Trutat" % (recordTitle)
+    return "%s - Fonds Trutat" % retrieve_title(record)
