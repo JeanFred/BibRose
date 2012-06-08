773664e8c20de5d1cc9c8944659a8333ea705405
Jean-Frédéric <JeanFred1@gmail.com>
2012-06-08 22:13:44 +0200

    Fix license and requirements in setup script

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
+#from distutils.core import setup
 
 setup(name='BibRose',
     version      = '0.1',
@@ -8,7 +9,7 @@ setup(name='BibRose',
     py_modules = ['OaiRecordHandling', 'OaiServerTools', 'GeneralProcess', 'CommonsFunctions'],
     author       = 'Jean-Frederic',
     author_email = 'jeanfred@github',
-    license      = 'Biopython License',
-    install_requires     = ['pyoai', 'pywikipedia'],
+    license      = 'GPL',
+    install_requires     = ['pyoai'],
     data_files=[('', ['oai_servers.cfg'])],
     )
