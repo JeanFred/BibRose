7b77fb7e498283e8de5bba7e6fc83271127367d6
Jean-Frédéric <JeanFred1@gmail.com>
2012-09-18 01:31:44 +0200

    Improvements in the GeneralProcess
    - Add method to write a dictionary in wiki format
    - Sort dictionary based on element count
    - Beautify count dictionaries updates

--- GeneralProcess.py
+++ GeneralProcess.py
@@ -7,7 +7,13 @@ __authors__ = 'User:Jean-Frédéric'
 
 
 from OaiRecordHandling import *
+import codecs
+import os
 
+
+FIELDS = ['publisher', 'description', 'language', 'format', 'type', 'rights',
+          'date', 'relation', 'source', 'coverage', 'contributor', 'title',
+          'identifier', 'creator', 'subject']
 
 def process_records1(records):
     """Process the records
@@ -37,39 +43,52 @@ def process_records2(records):
 
     Kind of an ad-hoc, all purpose method.
     """
-    print "%s records" % len(records)
-    #trutatRecords = filter(is_Trutat, records)
-    #print "%s records left after filtering" % len(trutatRecords)
     # Initialises the dictionaries
     totalDict = {}
+    master_dict = {}
     for field in FIELDS:
-        exec("%s_dict = {}" % (field))
+        master_dict[field] = {}
         totalDict[field] = 0
     #Iterates through the records
     for record in records:
         for field in FIELDS:
-            #print field
-            data = record[1][field]
-            myDict = eval("%s_dict" % field)
+            updateCountDict(master_dict[field], record[1][field])
             totalDict[field] += len(record[1][field])
-            # this is the ugly way, will beautify later
-            #updateCountDict(myDict,data)
+
     for field in FIELDS:
-        fileName = join('metadata', 'dict',  '%s_dict.csv' % field)
-        myDict = eval("%s_dict" % field)
-        #print mySet
-        #write_dict_to_disk(myDict, fileName)
-        #exec("print %s_counter" % (field))
+        fileName = os.path.join('metadata', 'dict',  '%s_dict.csv' % field)
+        write_dict_as_csv(master_dict[field], field, 'csv')
+        write_dict_as_wiki(master_dict[field], field, 'wiki')
+
     for k, v in totalDict.items():
         print k, v
 
 
-def updateCountDict(aDict, records):
-    for record in records:
-        if record in aDict:
-            aDict[record] += 1
+def write_dict_as_wiki(aDict, name, directory):
+    with codecs.open(os.path.join(directory, name), 'w', 'utf-8') as wikipage:
+        wikipage.write("{|\nItem | Count | Tag | Categories")               
+        items = aDict.items()
+        items.sort(key=lambda x: x[1], reverse=True)
+        for item in items:
+            #table_line = "\n| %s | %s | " % (item)
+            table_line = """
+{{User:Mk-II/AncelyRow
+|item       =%s
+|count      =%s
+|value      =
+|categories =
+}}""" % item
+        
+            wikipage.write(unicode(table_line))
+        wikipage.write("\n|}")
+
+
+def updateCountDict(aDict, items):
+    for item in items:
+        if item in aDict:
+            aDict[item] += 1
         else:
-            aDict[record] = 1
+            aDict[item] = 1
 
 
 def write_set_to_disk(mySet, fileName):
@@ -80,15 +99,15 @@ def write_set_to_disk(mySet, fileName):
         f.write("\n".join(mySet))
 
 
-def write_dict_to_disk(myDict, fileName):
+def write_dict_as_csv(myDict, name, directory):
     """Write a given dictionary on disk as CSV
     """
-    print "Writing dict to %s" % fileName
+    print "Writing dict as %s in %s" % (name, directory)
     items = myDict.items()
-
+    items.sort(key=lambda x: x[1], reverse=True)
     def unicode_join_tuple(item):
         return "|".join(map(unicode, item))
-    with codecs.open(fileName, 'w', 'utf-8') as f:
+    with codecs.open(os.path.join(directory, name + '.csv'), 'w', 'utf-8') as f:
         f.write("\n".join(map(unicode_join_tuple, items)))
 
 
@@ -96,16 +115,16 @@ def main():
     """Entry point
     """
     directory = 'ancely'
-    dump_all_records_from_server(directory, 'general:CL21')
+    #dump_all_records_from_server(directory, 'general:CL21')
     #arks = ['ark:/74899/B315556101_CP0004_09_002',
     #        'ark:/74899/B315556101_CP0004_09_009',
     #        'ark:/74899/B315556101_CP0004_09_014']
-    #print "Retrieving records from disk..."
-    #records = retrieve_records_from_disk('records')
-    #print "...done"
-    #print "Processing records..."
-    #process_records2(records)
-    #print "...done"
+    print "Retrieving records from disk..."
+    records = retrieve_records_from_disk('ancely')
+    print "...done"
+    print "Processing records..."
+    process_records2(records)
+    print "...done"    
     ##records = map(get_record_from_ARK,arks)
     ##dump_all_records_from_server("records")
 
