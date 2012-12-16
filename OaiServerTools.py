88cca6fa23e5f97354e3daa48b8c7029c0a3ce27
JeanFred <JeanFred@github>
2012-12-16 22:25:48 +0100

    Convert to class-based structure OaiServerTools

--- OaiServerTools.py
+++ OaiServerTools.py
@@ -5,36 +5,40 @@
 """
 __authors__ = 'User:Jean-Frédéric'
 
-import sys
 import ConfigParser
 
 from oaipmh.client import Client
 from oaipmh.metadata import MetadataRegistry, oai_dc_reader
 
-sys.path.append('..')
 
-oai_config = ConfigParser.SafeConfigParser()
-oai_config.read('oai_servers.cfg')
-current_config = 'ToulouseBis'
+class OaiClient:
 
-registry = MetadataRegistry()
-registry.registerReader('oai_dc', oai_dc_reader)
-client = Client(oai_config.get(current_config, 'url'), registry)
+    def __init__(self, configuration_file):
+        """Constructor."""
+        self.oai_config = ConfigParser.SafeConfigParser()
+        self.oai_config.read(configuration_file)
+        self.current_config = 'ToulouseBis'
 
+        registry = MetadataRegistry()
+        registry.registerReader('oai_dc', oai_dc_reader)
+        self.client = Client(self._get_config_value('url'), registry)
 
-def get_record_from_ARK(ark):
-    """Retrieve a OAI Record from the OAI server based on the ARK identifier
-    """
-    identifier_root = oai_config.get(current_config, 'identifier_root')
-    return client.getRecord(identifier='%s:/%s' % (identifier_root, ark),
-                            metadataPrefix='oai_dc')
+    def _get_config_value(self, field):
+        """Return the config value for the given field."""
+        return self.oai_config.get(self.current_config, field)
 
+    def get_record_from_ARK(self, ark):
+        """Retrieve a OAI Record from the OAI server based on the ARK."""
+        identifier = '%s:/%s' % (self._get_config_value('identifier_root'), ark)
+        return self.client.GetRecord(identifier=identifier,
+                                     metadataPrefix='oai_dc')
 
-def dump_all_records_from_server(directory, oai_set):
-    """Retrieve all records from distant server and dump it on disk
-    """
-    for record in client.listRecords(metadataPrefix='oai_dc', set=oai_set):
-        try:
-            pickle_record(record, directory)
-        except:
-            pass
+    def dump_all_records_from_server(self, directory, oai_set):
+        """Retrieve all records from distant server and dump it on disk."""
+        for record in self.client.ListRecords(metadataPrefix='oai_dc',
+                                              set=oai_set):
+            try:
+                oai_record = OaiRecord(record)
+                oai_record .pickle_record(directory)
+            except:
+                pass
