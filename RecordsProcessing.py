f131e7058f9647f2eb3490084141ea23f4bd1780
JeanFred <JeanFred@github>
2013-05-07 18:43:55 +0200

    Update main method code, title methods

--- RecordsProcessing.py
+++ RecordsProcessing.py
@@ -160,9 +160,9 @@ def main():
     print "...done"
     #processor.retrieve_metadata_from_records_for_alignment()
     print "Retrieving mapping from wikipages..."
-    mapping = pickle.load(open('ancely_alignment', 'r'))
+    #mapping = pickle.load(open('ancely_alignment', 'r'))
     processor.metadata_mapper = MetadataMapper()
-    processor.metadata_mapper.retrieve_metadata_alignments(['type', 'coverage', 'creator', 'subject'])
+    processor.metadata_mapper.retrieve_metadata_alignments(['type', 'coverage', 'creator', 'date', 'subject'])
     print "...done"
     print "Mapping records..."
 
@@ -170,18 +170,105 @@ def main():
     all_categories = Counter()
     categories_count_per_file = dict()
 
-    for (record_metadata, record_categories) in processor.loop_over_and_map():
-        all_categories.update(record_categories)
-        categories_count_per_file[record_metadata['identifier']] = len(record_categories)
-        print "= %s =" % record_metadata['identifier']
-        template_name = 'User:Jean-Frédéric/Ancely/Ingestion'.encode('utf-8')
-        tpl = textlib.glue_template_and_params((template_name,
-                                                record_metadata))
+    reader = processor.loop_over_and_map()
+    template_name = 'User:Jean-Frédéric/Ancely/Ingestion'.encode('utf-8')
+    front_titlefmt = ""
+    variable_titlefmt = "%(title)s"
+    rear_titlefmt = " - Fonds Ancely - %(identifier)s.%(_ext)s"
+        #upload_file(photo, categories)
+        #all_categories.update(record_categories)
+        #categories_count_per_file[record_metadata['identifier']] = len(record_categories)
+        #print "= %s =" % record_metadata['identifier']
+        #template_name = 'User:Jean-Frédéric/Ancely/Ingestion'.encode('utf-8')
+        #tpl = textlib.glue_template_and_params((template_name,
+                                                #record_metadata))
         #print "{{collapse|title=%s|1=<pre>\n%s\n</pre>}}" % (record_metadata['identifier'], tpl)
-        print tpl
-        print "<nowiki>\n%s\n</nowiki>" % make_categories(record_categories)
-        print "...done"
-    categorisation_statistics(all_categories, categories_count_per_file)
+        #print tpl
+        #print "<nowiki>\n%s\n</nowiki>" % make_categories(record_categories)
+
+        #upload_file(url=url,
+                    #description="\n\n".join([tpl, make_categories(record_categories)]),
+                    #title=title)
+
+        #print "...done"
+    #categorisation_statistics(all_categories, categories_count_per_file)
+    reader_slice = itertools.islice(reader, 282, 300)
+    uploadBot = CustomDataIngestionBot(reader=reader_slice,
+                                     front_titlefmt=front_titlefmt,
+                                     rear_titlefmt=rear_titlefmt,
+                                     variable_titlefmt=variable_titlefmt,
+                                     pagefmt=template_name)
+    print "Uploading..."
+    uploadBot.run()
+
+
+def _cut_title(fixed_front, variable, fixed_rear, MAX_LENGTH=255):
+    """Return the given title smartly cut"""
+    fixed_length = len(fixed_front) + len(fixed_rear)
+    available_length = MAX_LENGTH - fixed_length
+    chunked = variable.split()
+    part = 1
+    while len(variable) > available_length:
+        variable = " ".join(chunked[:-part]) + "..."
+        part += 1
+    return fixed_front + variable + fixed_rear
+
+
+def make_title(entries, fixed_front_fmt, fixed_rear_fmt, variable_fmt):
+    """Return a title based on the metadata and format strings.
+
+    This method uses three format strings: the fixed front, the variable,
+    and the fixed rear.
+
+    """
+    fixed_front = fixed_front_fmt % entries
+    fixed_rear = fixed_rear_fmt % entries
+    variable = variable_fmt % entries
+    return cleanUpTitle(_cut_title(fixed_front, variable, fixed_rear,
+                                   MAX_LENGTH=255))
+
+
+class CustomDataIngestionBot(DataIngestionBot):
+
+    """Overload of DataIngestionBot."""
+
+    def __init__(self, reader, front_titlefmt, rear_titlefmt,
+                 variable_titlefmt, pagefmt,
+                 site=pywikibot.getSite(u'commons', u'commons')):
+        self.reader = reader
+        self.front_titlefmt = front_titlefmt
+        self.rear_titlefmt = rear_titlefmt
+        self.variable_titlefmt = variable_titlefmt
+        self.pagefmt = pagefmt
+        self.site = site
+        #super(self.__class__, self).__init__()
+
+    def _doUpload(self, photo):
+        duplicates = photo.findDuplicateImages(self.site)
+        if duplicates:
+            pywikibot.output(u"Skipping duplicate of %r" % (duplicates, ))
+            return duplicates[0]
+
+        title = make_title(photo, self.front_titlefmt, self.rear_titlefmt,
+                           self.variable_titlefmt)
+
+        description = textlib.glue_template_and_params((self.pagefmt,
+                                                        photo.metadata))
+
+        bot = UploadRobot(url=photo.URL,
+                          description=description,
+                          useFilename=title,
+                          keepFilename=True,
+                          verifyDescription=True,
+                          uploadByUrl=False,
+                          targetSite=self.site)
+        bot._contents = photo.downloadPhoto().getvalue()
+        bot._retrieved = True
+
+        print title
+        print description
+        bot.run()
+        return title
 
 
 def categorisation_statistics(all_categories, categories_count_per_file):
@@ -211,6 +298,30 @@ def categorisation_statistics(all_categories, categories_count_per_file):
     except ImportError, e:
         "Numpy is needed to have categorisation statistics."
 
+
+def cleanUpTitle(title):
+    ''' Clean up the title of a potential mediawiki page. Otherwise the title of
+    the page might not be allowed by the software.
+
+    '''
+    title = title.strip()
+    title = re.sub(u"[<{\\[]", u"(", title)
+    title = re.sub(u"[>}\\]]", u")", title)
+    title = re.sub(u"[ _]?\\(!\\)", u"", title)
+    title = re.sub(u",:[ _]", u", ", title)
+    title = re.sub(u"[;:][ _]", u", ", title)
+    title = re.sub(u"[\t\n ]+", u" ", title)
+    title = re.sub(u"[\r\n ]+", u" ", title)
+    title = re.sub(u"[\n]+", u"", title)
+    title = re.sub(u"[?!]([.\"]|$)", u"\\1", title)
+    title = re.sub(u"[&#%?!]", u"^", title)
+    title = re.sub(u"[;]", u",", title)
+    title = re.sub(u"[/+\\\\:]", u"-", title)
+    title = re.sub(u"--+", u"-", title)
+    title = re.sub(u",,+", u",", title)
+    title = re.sub(u"[-,^]([.]|$)", u"\\1", title)
+    title = title.replace(u" ", u"_")
+    return title
 
 if __name__ == "__main__":
     main()
